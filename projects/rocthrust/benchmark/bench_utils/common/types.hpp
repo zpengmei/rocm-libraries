26feb414d5fea6a8254ff3e9dfa6a320409320c2
Sander Bos <sander@streamhpc.com>
2026-06-17 12:00:00 +0200

    refactor(rocthrust): remove pointless using statements of std types

--- projects/rocthrust/benchmark/bench_utils/common/types.hpp
+++ projects/rocthrust/benchmark/bench_utils/common/types.hpp
@@ -79,19 +79,4 @@ bool __host__ __device__ operator==(T const& lhs, large_data const& rhs)
 
 }; // namespace bench_utils
 
-using int8_t   = std::int8_t;
-using int16_t  = std::int16_t;
-using int32_t  = std::int32_t;
-using int64_t  = std::int64_t;
-using uint8_t  = std::uint8_t;
-using uint16_t = std::uint16_t;
-using uint32_t = std::uint32_t;
-using uint64_t = std::uint64_t;
-#if THRUST_BENCHMARKS_HAVE_INT128_SUPPORT
-using int128_t  = __int128_t;
-using uint128_t = __uint128_t;
-#endif
-using float32_t = float;
-using float64_t = double;
-
 #endif // ROCTHRUST_BENCHMARKS_BENCH_UTILS_TYPES_HPP_
