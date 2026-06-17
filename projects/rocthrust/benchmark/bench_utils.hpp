91fd7dd703be973f342e96d230ef508b8d2d8f2b
Sander Bos <sander@streamhpc.com>
2026-06-17 12:44:23 +0200

    fix(rocthrust): alias i128 and u128

--- projects/rocthrust/benchmark/bench_utils.hpp
+++ projects/rocthrust/benchmark/bench_utils.hpp
@@ -26,6 +26,11 @@
 
 #include <thrust/execution_policy.h>
 
+#ifndef _MSC_VER
+using int128_t  = __int128_t;
+using uint128_t = __uint128_t;
+#endif
+
 #include "generation_utils.hpp" // IWYU pragma: export
 
 /// This allows running rocThrust benchmarks with CCCL Thrust.
