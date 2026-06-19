e654b45e16582106f06349976db80f47ebe3c19f
NguyenNhuDi <zenguyen@amd.com>
2026-06-19 13:08:31 -0400

    updated shuffle to primbench

--- projects/rocthrust/benchmark/bench/shuffle/basic.cu
+++ projects/rocthrust/benchmark/bench/shuffle/basic.cu
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Copyright (c) 2011-2023, NVIDIA CORPORATION.  All rights reserved.
- * Modifications Copyright (c) 2024-2025, Advanced Micro Devices, Inc.  All rights reserved.
+ * Modifications Copyright (c) 2024-2026, Advanced Micro Devices, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -27,7 +27,7 @@
  ******************************************************************************/
 
 // Benchmark utils
-#include "../../bench_utils/bench_utils.hpp"
+#include "bench_utils.hpp"
 
 // rocThrust
 #include <thrust/device_vector.h>
@@ -35,161 +35,97 @@
 #include <thrust/random.h>
 #include <thrust/shuffle.h>
 
-// Google Benchmark
-#include <benchmark/benchmark.h>
-
-// STL
-#include <cstddef>
 #include <string>
-#include <vector>
 
-struct basic
+template <typename T>
+struct shuffle_benchmark : public primbench::benchmark_interface
 {
-  template <typename T, typename Policy>
-  double run(thrust::device_vector<T>& data, const std::string rng_engine, Policy policy)
+  shuffle_benchmark(size_t items, std::string rng_engine)
+      : m_items(items)
+      , rng_engine(rng_engine)
+  {}
+
+  primbench::json meta() const override
   {
+    return primbench::json{}
+      .add("algo", "shuffle")
+      .add("subalgo", "basic")
+      .add("input_type", primbench::name<T>())
+      .add("rng_engine", rng_engine)
+      .add("elements", m_items);
+  }
+
+  void run(primbench::state& state) override
+  {
+    bench_utils::caching_allocator_t alloc{};
+    thrust::detail::device_t policy{};
+
+    state.set_items(m_items);
+    state.add_reads<T>(m_items);
+    state.add_writes<T>(m_items);
+
+    thrust::device_vector<T> data(m_items);
     auto do_engine = [&](auto&& engine_constructor) {
-      thrust::shuffle(policy, data.begin(), data.end(), engine_constructor());
-
-      bench_utils::gpu_timer d_timer;
-
-      d_timer.start(0);
-      thrust::shuffle(policy, data.begin(), data.end(), engine_constructor());
-      d_timer.stop(0);
-
-      return d_timer.get_duration();
+      state.run([&] {
+        thrust::shuffle(policy, data.begin(), data.end(), engine_constructor());
+      });
     };
 
     if (rng_engine == "minstd")
     {
-      return do_engine([] {
+      do_engine([] {
         return thrust::random::minstd_rand{};
       });
     }
     else if (rng_engine == "ranlux24")
     {
-      return do_engine([] {
+      do_engine([] {
         return thrust::random::ranlux24{};
       });
     }
     else if (rng_engine == "ranlux48")
     {
-      return do_engine([] {
+      do_engine([] {
         return thrust::random::ranlux48{};
       });
     }
-    else if (rng_engine == "taus88")
+    else
     {
-      return do_engine([] {
+      do_engine([] {
         return thrust::random::taus88{};
       });
     }
-
-    return -1;
   }
+
+private:
+  size_t m_items;
+  std::string rng_engine;
 };
 
-template <class Benchmark, class T>
-void run_benchmark(
-  benchmark::State& state, const std::size_t elements, const std::string /*seed_type*/, const std::string rng_engine)
-{
-  // Benchmark object
-  Benchmark benchmark{};
-
-  // GPU times
-  std::vector<double> gpu_times;
-
-  // Generate input
-  thrust::device_vector<T> data(elements);
-
-  bench_utils::caching_allocator_t alloc{};
-  thrust::detail::device_t policy{};
-
-  for (auto _ : state)
-  {
-    double duration = benchmark.template run<T>(data, rng_engine, policy(alloc));
-    state.SetIterationTime(duration);
-    gpu_times.push_back(duration);
+#define QUEUE(T)                                            \
+  for (size_t size : bench_utils::sizes(2 * sizeof(T)))     \
+  {                                                         \
+    executor.queue<shuffle_benchmark<T>>(size, "minstd");   \
+    executor.queue<shuffle_benchmark<T>>(size, "ranlux24"); \
+    executor.queue<shuffle_benchmark<T>>(size, "ranlux48"); \
+    executor.queue<shuffle_benchmark<T>>(size, "taus88");   \
   }
-
-  // BytesProcessed include read and written bytes, so when the BytesProcessed/s are reported
-  // it will actually be the global memory bandwidth gotten.
-  state.SetBytesProcessed(state.iterations() * 2 * elements * sizeof(T));
-  state.SetItemsProcessed(state.iterations() * elements);
-
-  const double gpu_cv         = bench_utils::StatisticsCV(gpu_times);
-  state.counters["gpu_noise"] = gpu_cv;
-}
-
-#define CREATE_BENCHMARK(T, RngEngine, Elements)                                      \
-  benchmark::RegisterBenchmark(                                                       \
-    bench_utils::bench_naming::format_name(                                           \
-      "{algo:shuffle,subalgo:" + name + ",input_type:" #T + ",rng_engine:" #RngEngine \
-      + ",elements:" + bench_utils::format_pow2(Elements))                            \
-      .c_str(),                                                                       \
-    run_benchmark<Benchmark, T>,                                                      \
-    Elements,                                                                         \
-    seed_type,                                                                        \
-    RngEngine)
-
-#define BENCHMARK_RNG_ENGINE(type, rng_engine)             \
-  for (size_t size : bench_utils::sizes(sizeof(type)))     \
-    bs.push_back(CREATE_BENCHMARK(type, rng_engine, size));
-
-#define BENCHMARK_TYPE(type)             \
-  BENCHMARK_RNG_ENGINE(type, "minstd")   \
-  BENCHMARK_RNG_ENGINE(type, "ranlux24") \
-  BENCHMARK_RNG_ENGINE(type, "ranlux48") \
-  BENCHMARK_RNG_ENGINE(type, "taus88")
-
-template <class Benchmark>
-void add_benchmarks(
-  const std::string& name, std::vector<benchmark::internal::Benchmark*>& benchmarks, const std::string seed_type)
-{
-  std::vector<benchmark::internal::Benchmark*> bs;
-  BENCHMARK_TYPE(int8_t)
-  BENCHMARK_TYPE(int16_t)
-  BENCHMARK_TYPE(int32_t)
-  BENCHMARK_TYPE(int64_t)
-#ifndef _MSC_VER
-  BENCHMARK_TYPE(int128_t)
-#endif
-
-  benchmarks.insert(benchmarks.end(), bs.begin(), bs.end());
-}
 
 int main(int argc, char* argv[])
 {
-  cli::Parser parser(argc, argv);
-  parser.set_optional<std::string>("name_format", "name_format", "human", "either: json,human,txt");
-  parser.set_optional<std::string>("seed", "seed", "random", bench_utils::get_seed_message());
-  parser.run_and_exit_if_error();
+  primbench::settings settings;
+  settings.size                 = 1; // bench_utils::sizes() calculates it later.
+  settings.min_gpu_ms_per_batch = 100;
+  primbench::executor executor(argc, argv, settings, primbench::flags::sync);
 
-  // Parse argv
-  benchmark::Initialize(&argc, argv);
-  bench_utils::bench_naming::set_format(parser.get<std::string>("name_format")); /* either: json,human,txt */
-  const std::string seed_type = parser.get<std::string>("seed");
+  QUEUE(int8_t)
+  QUEUE(int16_t)
+  QUEUE(int32_t)
+  QUEUE(int64_t)
 
-  // Benchmark info
-  bench_utils::add_common_benchmark_info();
-  benchmark::AddCustomContext("seed", seed_type);
+#ifndef _MSC_VER
+  QUEUE(int128_t)
+#endif
 
-  // Add benchmark
-  std::vector<benchmark::internal::Benchmark*> benchmarks;
-  add_benchmarks<basic>("basic", benchmarks, seed_type);
-
-  // Use manual timing
-  for (auto& b : benchmarks)
-  {
-    b->UseManualTime();
-    b->Unit(benchmark::kMicrosecond);
-    b->MinTime(0.4); // in seconds
-  }
-
-  // Run benchmarks
-  benchmark::RunSpecifiedBenchmarks(bench_utils::ChooseCustomReporter());
-
-  // Finish
-  benchmark::Shutdown();
-  return 0;
+  executor.run();
 }
